424136f0427be5edada6e9dc8b4aab5913d44625
MVMAM <ediconpts9@gmail.com>
2023-11-14 21:08:09 +0500

    some changes

--- DRF/people/serializer.py
+++ DRF/people/serializer.py
@@ -15,8 +15,8 @@ from .models import People
 class PeopleSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
     content = serializers.CharField()
-    time_create = serializers.DateTimeField()
-    time_update = serializers.DateTimeField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
     is_published = serializers.BooleanField(default=True)
     cat_id = serializers.IntegerField()
 
--- DRF/people/views.py
+++ DRF/people/views.py
@@ -14,13 +14,16 @@ class PeopleAPIView(APIView):
         return Response({'posts': PeopleSerializer(w, many=True).data})
 
     def post(self, request):
+        serializer = PeopleSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
         post_new = People.objects.create(
             title=request.data["title"],
             content=request.data["content"],
             cat_id=request.data["cat_id"]
         )
 
-        return Response({'post': model_to_dict(post_new)})
+        return Response({'post': PeopleSerializer(post_new).data})
 
 
 # class PeopleAPIView(generics.ListAPIView):
