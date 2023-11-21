2eb696c8f7ea9b6f5fbe38e8a809b73caff59159
MVMAM <ediconpts9@gmail.com>
2023-11-21 21:30:47 +0500

    last commit

--- DRF/people/views.py
+++ DRF/people/views.py
@@ -1,16 +1,24 @@
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework import generics
 
 from .serializer import PeopleSerializer
 from .permissions import *
-from .models import People, Category
+from .models import People
+
+
+class PeopleAPIListPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 2
 
 
 class PeopleAPIList(generics.ListCreateAPIView):
     queryset = People.objects.all()
     serializer_class = PeopleSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
+    pagination_class = PeopleAPIListPagination
 
 
 class PeopleAPIUpdate(generics.RetrieveUpdateAPIView):
